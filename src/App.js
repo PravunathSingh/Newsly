a8661426ac5e800c812acb7f12edfad9e0fe85ea
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-09 22:55:28 +0530

    Added the main App files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/layout/Navbar'
+import News from './components/news/News'
+import USNews from './components/news/USNews/USNews'
+import InNews from './components/news/InNews/InNews'
+import BBCNews from './components/news/BBCNews/BBCNews'
+import Categories from './components/news/Categories/Categories'
+import Business from './components/news/Categories/Business/Business'
+import {Provider} from './context'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <Navbar />
+          <Switch>
+            <div className="container">
+              <Route exact path="/" component={News} />
+              <Route exact path="/USNews" component={USNews} />
+              <Route exact path="/InNews" component={InNews} />
+              <Route exact path="/BBCNews" component={BBCNews} />
+              <Route exact path="/Categories" component={Categories} />
+              <Route exact path="/Categories/Business" component={Business} />
+            </div>
+          </Switch>
+        </Router>
+      </Provider>
+      
+      
+    )
+  }
 }
 
 export default App;
