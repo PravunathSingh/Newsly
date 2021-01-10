e6919efca21ad7c046e77c5a0b17042b22bfe2cd
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-10 13:25:04 +0530

    Added the App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
