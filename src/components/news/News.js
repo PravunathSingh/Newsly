a3bb6c7a3dfcc4cbf67f150bffaea69715c50633
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-10 11:55:50 +0530

    Added the News.js

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,31 +1,36 @@
 import React, { Component } from 'react'
+import Search from './Search/Search'
 import {Link} from 'react-router-dom';
 
 class News extends Component {
     render() {
         return (
-            <div className="row">
-                <div className="col-md-6">
-                    <div className="card p-3 mb-4 shadow-sm">
-                        <Link to="/USNews" className="news">Top Headlines From The United States</Link>
+            <React.Fragment>
+                <Search/>
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="card p-3 mb-4 shadow">
+                            <Link to="/USNews" className="news">Top Headlines From The United States</Link>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card p-3 mb-4 shadow">
+                            <Link to="/InNews" className="news">Top Headlines From India</Link>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card p-3 mb-4 shadow">
+                            <Link to="/BBCNews" className="news">Top Headlines From BBC</Link>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card p-3 mb-4 shadow">
+                            <Link to="/Categories" className="news">Top Headlines Across Different Categories</Link>
+                        </div>
                     </div>
                 </div>
-                <div className="col-md-6">
-                    <div className="card p-3 mb-4 shadow-sm">
-                        <Link to="/InNews" className="news">Top Headlines From India</Link>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="card p-3 mb-4 shadow-sm">
-                        <Link to="/BBCNews" className="news">Top Headlines From BBC</Link>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="card p-3 mb-4 shadow-sm">
-                        <Link to="/Categories" className="news">Top Headlines Across Different Categories</Link>
-                    </div>
-                </div>
-            </div>
+            </React.Fragment>
+            
         )
     }
 }
