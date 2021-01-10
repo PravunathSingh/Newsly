a50c2351c3761865e85b6049bea9f6048641adf8
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-10 11:53:34 +0530

    Added the context.js

---
+++ src/context.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+    state = {
+        news: [],
+        heading: ''
+    }
+
+    render() {
+        return (
+            <Context.Provider value={this.state}>
+                {this.props.children}
+            </Context.Provider>
+                
+        )
+    }
+}
+
+export const Consumer = Context.Consumer;
